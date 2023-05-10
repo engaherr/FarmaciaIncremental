e054c26de3c55a751e13ae5e4a1909ba9ce8fdfa
Dylan00Platas <131178283+Dylan00Platas@users.noreply.github.com>
2023-05-10 11:58:26 -0600

    Vectores, Implementación de menú de reaquisición

--- src/javafxfarmacia/controladores/FXMLMenuPrincipalAdminController.java
+++ src/javafxfarmacia/controladores/FXMLMenuPrincipalAdminController.java
@@ -6,7 +6,13 @@ package javafxfarmacia.controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafxfarmacia.utils.Utilidades;
 
 /**
  * FXML Controller class
@@ -15,6 +21,9 @@ import javafx.fxml.Initializable;
  */
 public class FXMLMenuPrincipalAdminController implements Initializable {
 
+    @FXML
+    private Label lbTitulo;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +31,30 @@ public class FXMLMenuPrincipalAdminController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void clicInventario(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicReadquisicion(ActionEvent event) {
+        Stage escenarioReadquisicion = new Stage();
+        escenarioReadquisicion.setScene(Utilidades.inicializaEscena("vistas/FXMLReadquisicion.fxml"));
+        escenarioReadquisicion.setTitle("Readquisicion");
+        escenarioReadquisicion.initModality(Modality.APPLICATION_MODAL);
+        escenarioReadquisicion.show();
+    }
+
+    @FXML
+    private void clicAbasto(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicPromociones(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicCerrarSesion(ActionEvent event) {
+    }
     
 }
