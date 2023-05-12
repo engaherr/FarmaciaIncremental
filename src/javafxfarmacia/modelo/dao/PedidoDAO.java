5ea10cb402649a0bf30dc85dd719b2a2531ef1a1
Dylan00Platas <131178283+Dylan00Platas@users.noreply.github.com>
2023-05-11 19:43:48 -0600

    creación de setter y getters

--- src/javafxfarmacia/modelo/dao/PedidoDAO.java
+++ src/javafxfarmacia/modelo/dao/PedidoDAO.java
@@ -7,7 +7,10 @@ package javafxfarmacia.modelo.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import javafxfarmacia.modelo.ConexionBD;
+import javafxfarmacia.modelo.pojo.Pedido;
 import javafxfarmacia.modelo.pojo.PedidoRespuesta;
 import javafxfarmacia.utils.Constantes;
 
@@ -29,18 +32,13 @@ public class PedidoDAO {
                 ResultSet resultado = prepararSentencia.executeQuery();
                 ArrayList<Pedido> pedidoConsulta = new ArrayList();
                 while(resultado.next()){
-                     pedido.setIdpedido(resultado.getInt("idPedido"));
-                    pedido.setFecha_Pedido(resultado.getString("fecha_pedido"));
-                    pedido.setFecha_Entrega(resultado.getString("fecha_entrega"));
+                    Pedido pedido = new Pedido();
+                    pedido.setIdPedido(resultado.getInt("idPedido"));
+                    pedido.setFecha_pedido(resultado.getString("fecha_pedido"));
+                    pedido.setFecha_entrega(resultado.getString("fecha_entrega"));
                     pedido.setNombre(resultado.getString("nombre"));
                     pedido.setCantidad(resultado.getInt("cantidad"));
-                    pedido.setEstad(resultado.getString("estado"));
-
-                    pedido.setVentaControlada(resultado.getBoolean("ventaControlada"));
-                    pedido.setIdSucursal(resultado.getInt("sucursal_idSucursal"));
-                    pedido.setNombreSucursal(resultado.getString("nombreSucursal"));
-                    pedido.setCantidad(resultado.getInt("cantidad"));
-                    pedido.setPresentacion(resultado.getString("presentacion"));
+                    pedido.setEstado(resultado.getString("estado"));
                     pedidoConsulta.add(pedido);
 
                 }
@@ -56,43 +54,5 @@ public class PedidoDAO {
     }
      
      
-     public static PedidoRespuesta obtenerInformacionPedido(){
-        PedidoRespuesta respuesta = new PedidoRespuesta();
-        respuesta.setCodigoRespuesta(Constantes.OPERACION_EXITOSA);
-        Connection conexionBD = ConexionBD.abrirConexionBD();
-        if(conexionBD != null){
-            try {
-                String consulta = "SELECT p.idpedido, p.estado, p.fecha_pedido, p.fecha_entrega, p.cantidad, pr.nombre \n" +
-"FROM pedidos p \n" +
-"JOIN producto pr ON p.idProducto = pr.idProducto;";
-                PreparedStatement prepararSentencia = conexionBD.prepareStatement(consulta);
-                ResultSet resultado = prepararSentencia.executeQuery();
-                ArrayList<Pedido> pedidoConsulta = new ArrayList();
-                while(resultado.next()){
-                     pedido.setIdpedido(resultado.getInt("idPedido"));
-                    pedido.setFecha_Pedido(resultado.getString("fecha_pedido"));
-                    pedido.setFecha_Entrega(resultado.getString("fecha_entrega"));
-                    pedido.setNombre(resultado.getString("nombre"));
-                    pedido.setCantidad(resultado.getInt("cantidad"));
-                    pedido.setEstad(resultado.getString("estado"));
-
-                    pedido.setVentaControlada(resultado.getBoolean("ventaControlada"));
-                    pedido.setIdSucursal(resultado.getInt("sucursal_idSucursal"));
-                    pedido.setNombreSucursal(resultado.getString("nombreSucursal"));
-                    pedido.setCantidad(resultado.getInt("cantidad"));
-                    pedido.setPresentacion(resultado.getString("presentacion"));
-                    pedidoConsulta.add(pedido);
-
-                }
-                respuesta.setPedidos(pedidoConsulta);
-                conexionBD.close();
-            } catch (SQLException e) {
-                respuesta.setCodigoRespuesta(Constantes.ERROR_CONSULTA);
-            }
-        }else{
-            respuesta.setCodigoRespuesta(Constantes.ERROR_CONEXION);
-        }
-        return respuesta;
-    }
-     
+   
 }
--- src/javafxfarmacia/modelo/pojo/Pedido.java
+++ src/javafxfarmacia/modelo/pojo/Pedido.java
@@ -10,69 +10,73 @@ package javafxfarmacia.modelo.pojo;
  */
 public class Pedido {
 
-    private final int idPedido;
-    private final String nombre;
-    private final String fecha_pedido;
-    private final String fecha_entrega;
-    private final int cantidad;
-    private final String estado;
+    private int idPedido;
+    private String nombre;
+    private String fecha_pedido;
+    private String fecha_entrega;
+    private int cantidad;
+    private String estado;
     
+    public Pedido() {
+    }
 
-public Pedido (int idPedido, String nombre,String fecha_pedido, String fecha_entrega, int cantidad, String estado) {
-    this.idPedido = idPedido;
-    this.nombre = nombre;
-    this.fecha_pedido = fecha_pedido;
-    this.fecha_entrega = fecha_entrega;
-    this.cantidad = cantidad;
-    this.estado = estado;
-}
-
+    public Pedido(int idPedido, String nombre, String fecha_pedido, String fecha_entrega, int cantidad, String estado) {
+        this.idPedido = idPedido;
+        this.nombre = nombre;
+        this.fecha_pedido = fecha_pedido;
+        this.fecha_entrega = fecha_entrega;
+        this.cantidad = cantidad;
+        this.estado = estado;
+    }
+    
     public int getIdPedido() {
         return idPedido;
     }
-
+    
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+    
     public String getNombre() {
         return nombre;
     }
-
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     public String getFecha_pedido() {
         return fecha_pedido;
     }
-
+    
+    public void setFecha_pedido(String fecha_pedido) {
+        this.fecha_pedido = fecha_pedido;
+    }
+    
     public String getFecha_entrega() {
         return fecha_entrega;
     }
-
+    
+    public void setFecha_entrega(String fecha_entrega) {
+        this.fecha_entrega = fecha_entrega;
+    }
+    
     public int getCantidad() {
         return cantidad;
     }
-
+    
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
     public String getEstado() {
         return estado;
     }
-
-    public int setIdPedido() {
-        return idPedido;
+    
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
-
-    public String setNombre() {
-        return nombre;
-    }
-
-    public String setFecha_pedido() {
-        return fecha_pedido;
-    }
-
-    public String setFecha_entrega() {
-        return fecha_entrega;
-    }
-
-    public int setCantidad() {
-        return cantidad;
-    }
-
-    public String setEstado() {
-        return estado;
-    }
-
 }
+
+
+
