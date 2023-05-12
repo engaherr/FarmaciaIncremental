250e671d4c897a253b86eadeca0b8d38977de2b1
Dylan00Platas <131178283+Dylan00Platas@users.noreply.github.com>
2023-05-11 19:50:51 -0600

    Creación de controladores

--- src/javafxfarmacia/controladores/FXMLConsultarPedidosController.java
+++ src/javafxfarmacia/controladores/FXMLConsultarPedidosController.java
@@ -6,7 +6,10 @@ package javafxfarmacia.controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -14,6 +17,21 @@ import javafx.fxml.Initializable;
  * @author dplat
  */
 public class FXMLConsultarPedidosController implements Initializable {
+
+    @FXML
+    private TableView<?> tvPedidos;
+    @FXML
+    private TableColumn<?, ?> colPedido;
+    @FXML
+    private TableColumn<?, ?> colEstado;
+    @FXML
+    private TableColumn<?, ?> colProducto;
+    @FXML
+    private TableColumn<?, ?> colCantidad;
+    @FXML
+    private TableColumn<?, ?> colFechaPedido;
+    @FXML
+    private TableColumn<?, ?> colFechaEntrega;
 
     /**
      * Initializes the controller class.
