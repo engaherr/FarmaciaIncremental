a12b578198a58c8dbaacef152b5999ed1555f83a
Dylan00Platas <131178283+Dylan00Platas@users.noreply.github.com>
2023-05-11 19:45:34 -0600

    correción de setters

--- src/javafxfarmacia/modelo/pojo/PedidoRespuesta.java
+++ src/javafxfarmacia/modelo/pojo/PedidoRespuesta.java
@@ -34,7 +34,7 @@ public class PedidoRespuesta {
         return pedido;
     }
 
-    public void setProductos(ArrayList<Pedido> pedido) {
+    public void setPedidos(ArrayList<Pedido> pedido) {
         this.pedido = pedido;
     }
     
