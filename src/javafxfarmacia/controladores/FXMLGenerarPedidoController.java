e29f6e3ef7268416ce03ddd80b598bf43571a144
Dylan00Platas <131178283+Dylan00Platas@users.noreply.github.com>
2023-05-12 22:05:37 -0600

    Primera Implementación de combo box "producto"
    Necesita corrección

--- src/javafxfarmacia/controladores/FXMLGenerarPedidoController.java
+++ src/javafxfarmacia/controladores/FXMLGenerarPedidoController.java
@@ -1,26 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package javafxfarmacia.controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafxfarmacia.modelo.dao.TipoProductoDAO;
+import javafxfarmacia.modelo.pojo.Tipo;
+import javafxfarmacia.modelo.pojo.TipoRespuesta;
+import javafxfarmacia.utils.Constantes;
+import javafxfarmacia.utils.Utilidades;
 
-/**
- * FXML Controller class
- *
- * @author dplat
- */
 public class FXMLGenerarPedidoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private ComboBox<Tipo> cbTipo;
+
+    private ObservableList<Tipo> tipos;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        cargarInformacionTipo();
+    }
+
+    private void cargarInformacionTipo() {
+        tipos = FXCollections.observableArrayList();
+        TipoRespuesta tiposBD = TipoProductoDAO.obtenerInformacionTipo();
+        switch (tiposBD.getCodigoRespuesta()) {
+            case Constantes.ERROR_CONEXION:
+                Utilidades.mostrarDialogoSimple("Error de conexión", "Error de conexión con la base de datos", Alert.AlertType.ERROR);
+                break;
+
+            case Constantes.ERROR_CONSULTA:
+                Utilidades.mostrarDialogoSimple("Error de consulta", "Por el momento no se puede mostrar la información", Alert.AlertType.WARNING);
+                break;
+
+            case Constantes.OPERACION_EXITOSA:
+                tipos.addAll(tiposBD.getTipos());
+                cbTipo.setItems(tipos);
+                break;
+        }
+    }
 }
---
+++ src/javafxfarmacia/modelo/pojo/Tipo.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafxfarmacia.modelo.pojo;
+
+/**
+ *
+ * @author dplat
+ */
+
+ 
+public class Tipo {
+    private int idProducto;
+    private String nombre;
+
+    public Tipo() {
+    }
+
+    public Tipo(int idProducto, String nombre) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String toString(){
+        return nombre;
+    }
+}
+
+
