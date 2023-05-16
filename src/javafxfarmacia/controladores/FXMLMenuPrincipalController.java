335acd156012ada312a3c9d6d786163562580802
engaherr <zs21013858@estudiantes.uv.mx>
2023-05-15 19:18:56 -0600

    Menú Principal Encargado

---
+++ src/javafxfarmacia/controladores/FXMLMenuPrincipalController.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxfarmacia.controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafxfarmacia.utils.Utilidades;
+
+/**
+ * FXML Controller class
+ *
+ * @author kikga
+ */
+public class FXMLMenuPrincipalController implements Initializable {
+
+    @FXML
+    private Label lbTitulo;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+
+
+
+
+    @FXML
+    private void clicPromociones(ActionEvent event) {
+        Stage escenarioPromociones = (Stage) lbTitulo.getScene().getWindow();
+        escenarioPromociones.setScene(Utilidades.inicializaEscena("vistas/FXMLPromociones.fxml"));
+        escenarioPromociones.setTitle("Lista de promociones");
+        escenarioPromociones.initModality(Modality.APPLICATION_MODAL);
+        escenarioPromociones.showAndWait();
+        
+   }
+
+    @FXML
+    private void clicCerrarSesion(ActionEvent event) {
+         Utilidades.mostrarDialogoSimple("Cierre de sesión",
+            "Adiós usuario,vuelva pronto",
+            Alert.AlertType.INFORMATION);
+        Stage escenarioBase = (Stage) lbTitulo.getScene().getWindow();
+        escenarioBase.setScene(Utilidades.inicializaEscena("vistas/FXMLInicioSesion.fxml"));
+        escenarioBase.setTitle("Inicio de Sesión");
+        escenarioBase.show();
+    }
+    
+}
